d30eba13d523ec6d53a0222142bcd95d8c3ac8bc
Ranajit305 <nathranajit6@gmail.com>
2025-04-16 11:35:03 +0530

    update

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosUrl = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : import.meta.env.CLIENT_URL,
+    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : import.meta.env.BASE_URL,
     withCredentials: true
 })
