1e38de2812fa0388da4f23f11915cff78a1a1833
Ranajit305 <nathranajit6@gmail.com>
2025-04-16 11:18:54 +0530

    update

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosUrl = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : import.meta.env.BASE_URL,
+    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : 'fintrack-production-8853.up.railway.app/api',
     withCredentials: true
 })
